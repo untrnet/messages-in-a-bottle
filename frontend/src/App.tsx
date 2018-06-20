715c6027bdad1b03f540a0113dbf38272cba4fed
tbscanlon <thomas.scanlon@outlook.com>
2018-06-20 18:10:41 +0100

    Add bulma & bloomer for styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,13 @@
+import { Container, Title } from "bloomer";
 import * as React from 'react';
-
-import './App.css';
-import logo from './logo.svg';
 
 class App extends React.Component {
   public render(): JSX.Element {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Container>
+          <Title tag="h1" isSize={1}>Messages in a Bottle</Title>
+        </Container>
       </div>
     );
   }
