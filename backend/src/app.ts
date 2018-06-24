be36aada2c419bd9c797da5ff4e3d28a0b8d7f3f
tbscanlon <thomas.scanlon@outlook.com>
2018-06-24 14:53:33 +0100

    Add scaffolding for express-powered backend

---
+++ backend/src/app.ts
@@ -0,0 +1,82 @@
+import Express from "express";
+import { Server } from "http";
+
+/**
+ * Represents the "Messages in a Bottle" API in its entirety.
+ */
+export class App {
+    private app: Express.Application;
+    private server: Server;
+
+    constructor(
+        private NODE_ENV = process.env.NODE_ENV || "development",
+        private PORT     = process.env.PORT || "8080",
+    ) {
+        this.app = Express();
+
+        this.setEnvironment(this.NODE_ENV);
+
+        this.server = this.start(this.PORT);
+    }
+
+    /**
+     * Creates a new instance of the application.
+     * @returns {Server} The application's HTTP interface.
+     */
+    public static create(): Server {
+        return new this().server;
+    }
+
+    /**
+     * Retrieves the port the server is listening on.
+     * @returns {string} The port number.
+     */
+    public get port(): string {
+        return this.PORT;
+    }
+
+    /**
+     * Retrieves the environment the server is configured for.
+     * @returns {string} The environment name.
+     */
+    public get environment(): string {
+        return this.NODE_ENV;
+    }
+
+    /**
+     * Stops the server process.
+     */
+    public stop(): void {
+        this.server.close();
+    }
+
+    /**
+     * @private
+     * Sets what environment the server is running in.
+     * @param env The environment in which the app is running (e.g.
+     * production)
+     */
+    private setEnvironment(env: string): void {
+        if (env === "development") {
+            // dev specific env dode
+        } else {
+            // prod-only code
+        }
+    }
+
+    /**
+     * Adds the applications routes to the server.
+     */
+    private setRoutes(): void {
+        // route adding goes here.
+    }
+
+    /**
+     * Launches the server.
+     * @param port The port number the server should listen for requests on.
+     * @returns {Server} the configured & launched express server.
+     */
+    private start(port: string): Server {
+        return this.app.listen(port);
+    }
+}
---
+++ backend/test/app.test.ts
@@ -0,0 +1,69 @@
+import request from "supertest";
+import { Server } from "http";
+
+import { App } from "../src/app";
+
+describe("App", () => {
+    let instance: Server;
+    let app: App;
+    let res: request.Response;
+
+    describe("Initialisation", () => {
+        describe("Default configuration", () => {
+            beforeEach(() => {
+                process.env = {};
+                app = new App();
+            });
+
+            afterEach(() => {
+                app.stop();
+            });
+
+            it("Listens on port 8080", () => {
+                expect(app.port).toBe("8080");
+            });
+
+            it("Is set to development environment", () => {
+                expect(app.environment).toBe("development");
+            });
+        });
+
+        describe("Custom configuration", () => {
+            beforeEach(() => {
+                process.env = {
+                    NODE_ENV: "test",
+                    PORT: "3000"
+                };
+
+                app = new App();
+            });
+
+            afterEach(() => {
+                app.stop();
+            });
+
+            it("Listens on the specified port", () => {
+                expect(app.port).toBe(process.env.PORT);
+            });
+
+            it("Is set to the specified environment", () => {
+                expect(app.environment).toBe(process.env.NODE_ENV);
+            });
+        });
+    });
+
+    describe("GET /", () => {
+        beforeEach(() => {
+            instance = App.create();
+        });
+
+        afterEach(() => {
+            instance.close();
+        });
+
+        it("returns a 404 error", async () => {
+            res = await request(instance).get("/");
+            expect(res.status).toBe(404);
+        });
+    });
+});
