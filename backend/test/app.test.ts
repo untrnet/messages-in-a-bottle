9135983ae40afe0aaa66fb1503f3fcfeeeb128bc
tbscanlon <thomas.scanlon@outlook.com>
2018-06-24 19:55:02 +0100

    Change spacing to 2 spaces

--- backend/test/app.test.ts
+++ backend/test/app.test.ts
@@ -4,66 +4,70 @@ import { Server } from "http";
 import { App } from "../src/app";
 
 describe("App", () => {
-    let instance: Server;
-    let app: App;
-    let res: request.Response;
+  let instance: Server;
+  let app: App;
+  let res: request.Response;
 
-    describe("Initialisation", () => {
-        describe("Default configuration", () => {
-            beforeEach(() => {
-                process.env = {};
-                app = new App();
-            });
+  describe("Initialisation", () => {
+    describe("Default configuration", () => {
+      beforeEach(() => {
+        process.env = {};
+        app = new App();
+      });
 
-            afterEach(() => {
-                app.stop();
-            });
+      afterEach(() => {
+        app.stop();
+      });
 
-            it("Listens on port 8080", () => {
-                expect(app.port).toBe("8080");
-            });
+      it("Listens on port 8080", () => {
+        expect(app.port).toBe("8080");
+      });
 
-            it("Is set to development environment", () => {
-                expect(app.environment).toBe("development");
-            });
-        });
-
-        describe("Custom configuration", () => {
-            beforeEach(() => {
-                process.env = {
-                    NODE_ENV: "test",
-                    PORT: "3000"
-                };
-
-                app = new App();
-            });
-
-            afterEach(() => {
-                app.stop();
-            });
-
-            it("Listens on the specified port", () => {
-                expect(app.port).toBe(process.env.PORT);
-            });
-
-            it("Is set to the specified environment", () => {
-                expect(app.environment).toBe(process.env.NODE_ENV);
-            });
-        });
+      it("Is set to development environment", () => {
+        expect(app.environment).toBe("development");
+      });
     });
 
-    describe("GET /", () => {
-        beforeEach(() => {
-            instance = App.create();
-        });
+    describe("Custom configuration", () => {
+      beforeEach(() => {
+        process.env = {
+          NODE_ENV: "test",
+          PORT: "3000"
+        };
 
-        afterEach(() => {
-            instance.close();
-        });
+        app = new App();
+      });
 
+      afterEach(() => {
+        app.stop();
+      });
+
+      it("Listens on the specified port", () => {
+        expect(app.port).toBe(process.env.PORT);
+      });
+
+      it("Is set to the specified environment", () => {
+        expect(app.environment).toBe(process.env.NODE_ENV);
+      });
+    });
+  });
+
+  describe("Routes", () => {
+    beforeEach(() => {
+      instance = App.create();
+    });
+
+    afterEach(() => {
+      instance.close();
+    });
+
+    describe("/", () => {
+      describe("GET", () => {
         it("returns a 404 error", async () => {
-            res = await request(instance).get("/");
-            expect(res.status).toBe(404);
+          res = await request(instance).get("/");
+          expect(res.status).toBe(404);
         });
+      });
     });
+  });
 });
