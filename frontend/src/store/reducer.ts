338b5bff7a891d1fee31b3e7b5cfb053afb097da
tbscanlon <thomas.scanlon@outlook.com>
2018-06-26 17:27:10 +0100

    Add messages reducer to store

--- frontend/src/store/reducer.ts
+++ frontend/src/store/reducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import { authReducer as auth } from "./auth/reducer";
+import { messagesReducer as messages } from "./messages/reducer";
 import { uiReducer as ui } from "./ui/reducer";
 
 export default combineReducers({
   auth,
+  messages,
   ui
 });
