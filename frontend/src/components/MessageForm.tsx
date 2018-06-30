be2d0400d67e658c003394362e74b9aaaa708438
tbscanlon <thomas.scanlon@outlook.com>
2018-06-30 12:26:02 +0100

    Implement working message submission

---
+++ frontend/src/components/MessageForm.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { Field } from "redux-form";
+
+export const MessageForm = (props: any) => (
+  <div>
+    <form onSubmit={props.handleSubmit}>
+      <div className="field">
+        <label htmlFor="newMessage">Your message:</label>
+        <Field
+          name="newMessage"
+          component="textarea"
+          className="textarea is-primary"
+        />
+      </div>
+      <div className="field">
+        <button
+          type="submit"
+          className="button is-primary"
+        >
+          ok go
+      </button>
+      </div>
+    </form>
+  </div>
+);
--- frontend/src/containers/SubmitMessage.tsx
+++ frontend/src/containers/SubmitMessage.tsx
@@ -1,21 +1,13 @@
-import * as React from "react";
-import { Field, reduxForm } from "redux-form";
+import { reduxForm } from "redux-form";
 
-// const onSubmit = () => console.log("hello");
+import { store } from "../store";
+import { Actions } from "../store/messages/actions";
 
-const Form = (props: any) => (
-  <div>
-    <form onSubmit={props.handleSubmit}>
-      {/* <textarea name="ye" id="ye" cols={30} rows={10} /> */}
-      <label htmlFor="ye">Your message:</label>
-      <Field name="ye" component="input" type="text" />
-      <button type="submit" value="ok go" />
-    </form>
-  </div>
-);
+import { MessageForm } from "../components/MessageForm";
 
 const SubmitMessage = reduxForm({
-  form: "submitMessage"
-})(Form);
+  form: "submitMessage",
+  onSubmit: (values: any) => store.dispatch(Actions.Submit(values.newMessage))
+})(MessageForm);
 
 export default SubmitMessage;
