a7bcff50139a5853da3b789c54a2795815364248
tbscanlon <thomas.scanlon@outlook.com>
2018-07-19 15:51:13 +0100

    Add docker config & remove unnecessary complexity from code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,13 +12,11 @@ export class App {
   private server: Server;
 
   constructor(
-    private NODE_ENV = process.env.NODE_ENV || "development",
-    private PORT     = process.env.PORT     || "8080"
+    private PORT = 9002
   ) {
     this.app = Express();
     this.app.use(bodyparser.json());
 
-    this.setEnvironment(this.NODE_ENV);
     this.setRoutes();
 
     this.server = this.start(this.PORT);
@@ -33,43 +31,6 @@ export class App {
   }
 
   /**
-   * Retrieves the port the server is listening on.
-   * @returns {string} The port number.
-   */
-  public get port(): string {
-    return this.PORT;
-  }
-
-  /**
-   * Retrieves the environment the server is configured for.
-   * @returns {string} The environment name.
-   */
-  public get environment(): string {
-    return this.NODE_ENV;
-  }
-
-  /**
-   * Stops the server process.
-   */
-  public stop(): void {
-    this.server.close();
-  }
-
-  /**
-   * @private
-   * Sets what environment the server is running in.
-   * @param env The environment in which the app is running (e.g.
-   * production)
-   */
-  private setEnvironment(env: string): void {
-    if (env === "development") {
-      // dev specific env dode
-    } else {
-      // prod-only code
-    }
-  }
-
-  /**
    * Adds the applications routes to the server.
    */
   private setRoutes(): void {
@@ -81,7 +42,7 @@ export class App {
    * @param port The port number the server should listen for requests on.
    * @returns {Server} the configured & launched express server.
    */
-  private start(port: string): Server {
+  private start(port: number): Server {
     return this.app.listen(port);
   }
 }
